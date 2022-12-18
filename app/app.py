c0e8c9ad173fba9676c43d58cc3e97355334c7d9
Alicey <sirius@alicey.dev>
2022-12-19 06:57:20 +0900

    update データ永続化できないなんちゃってIFTTTの原型

--- app/app.py
+++ app/app.py
@@ -9,9 +9,9 @@ load_dotenv()
 app = Flask("simple-iot-server")
 app.logger.setLevel(logging.INFO)
 
-# want ['hw-name', 'addr', 'path', option['air', 'on']]
+# want ['hw-name', 'addr', 'path', option['air', 'on'], ['temp_upper', 27.5]]
 # task save
-send_tasks = {} #'hw-name':[['task-name', 'path', option['air', 'on']], ...]
+send_tasks = {} #'hw-name':[['task-name', output_hw, 'path', option['air', 'on']], ...]
 # >>> print(task)
 # {'hw_a': [['task_a', 'path_a/', ['air', 'on'], ['temp_upper', 27.5]], ['task_b', 'path_b/', ['air', 'off'], ['temp_under', 20]]], 'hw_z': [['task_z', 'path_z/', ['tv', 'on'], [実行条件]], ['task_z2', 'path_z/', ['tv', 'off'], [実行条件]]]}
 
@@ -23,7 +23,7 @@ res_hw = {} #'hw-name':'temp'
 
 @app.route("/")
 def top():
-    # remote_control('http://172.30.200.4:32121/api/send', 'air', 'cancel')
+    remote_control('http://172.30.200.4:32121/api/send', 'air', 'cancel')
     return jsonify({'status':'ok'}), 200
 
 ## API
@@ -46,13 +46,14 @@ def temp():
 
         # TaskDictを確認して、あれば実行
         if name in send_tasks:
+            app.logger.info(f"test!!!!!!!")
             tasks = send_tasks[name]
             for task in tasks:
-                if (task[4][0] == 'temp_upper' and float(task[4][1]) < temp) \
-                    or (task[4][0] == 'temp_under' and float(task[4][1]) > temp) \
-                    or (task[4][0] == 'hum_upper' and float(task[4][1]) < hum) \
-                    or (task[4][0] == 'hum_under' and float(task[4][1]) > hum):
-                    run_send_task(name, task)
+                if (task[4][0] == 'temp_upper' and float(task[4][1]) < float(temp)) \
+                    or (task[4][0] == 'temp_under' and float(task[4][1]) > float(temp)) \
+                    or (task[4][0] == 'hum_upper' and float(task[4][1]) < float(hum)) \
+                    or (task[4][0] == 'hum_under' and float(task[4][1]) > float(hum)):
+                    run_send_task(task)
                     app.logger.info(f"run task: {task}")
 
         #app.logger.info(hum,temp)
@@ -71,19 +72,20 @@ def remote_control(addr=None, hw=None, func=None):
     return False
 
 # task executer
-def run_send_task(hw_name, task):
+def run_send_task(task):
     try:
         task_name = task[0]
+        hw_name = task[1]
         task_kind = send_hw[hw_name][1]
         addr = send_hw[hw_name][0]
-        path = task[1]    
+        path = task[2]    
         option = task[3]
     except :
         app.logger.error('run_send_task: 引数が不正か、HW情報が存在しません')
         return True
     
-    if task_kind == 'temp':
-        remote_control(addr, option[0], option[1])
+    if task_kind == 'remote':
+        remote_control(f"{addr}/{path}", option[0], option[1])
     else:
         app.logger.error('run_send_task: 該当処理が存在しません')
         return True
@@ -111,7 +113,7 @@ def add_hw():
         hw_type = request.form.get('hw-type')
 
         if isws(name) or isws(addr) or isws(hw_type):
-            app.logger.info("フォームが空")
+            app.logger.info("add hw: フォームが空")
             return redirect(url_for('settings'))
         
         send_hw[name] = [addr, hw_type]
@@ -121,13 +123,28 @@ def add_hw():
 @app.route("/settings/add-rule/", methods=["GET","POST"])
 def add_rule():
     if request.method == 'GET':
-        return render_template('settings/add-rule.html')
+        return render_template('settings/add-rule.html', out_hws=send_hw, in_hws=res_hw)
     elif request.method == 'POST':
-        # name = request.form.get('name')
-        # addr = request.form.get('addr')
-        # hw_type = request.form.get('hw-type')
+        task_name = request.form.get('name')
+        input_hw = request.form.get('input_hw')
+        kind = request.form.get('kind')
+        number = request.form.get('number')
+        output_hw = request.form.get('output_hw')
+        path = request.form.get('path')
+        remote_name = request.form.get('remote_name') #Option
+        remote_func = request.form.get('remote_func') #Option
+
+        if isws(task_name) or isws(input_hw) or  isws(kind) or isws(number) or isws(output_hw) or isws(path) or isws(remote_name) or isws(remote_func):
+            app.logger.info("add rule: フォームが空")
+            return redirect(url_for('settings'))
+
+        if send_tasks.get(input_hw) is not None:
+            input_hw_temp = send_tasks.get(input_hw)
+        else:
+            input_hw_temp = []
+        input_hw_temp.append([task_name, output_hw, path, [remote_name, remote_func], [kind, number]])
+        send_tasks[input_hw] = input_hw_temp
         
-        # send_hw[name] = [addr, hw_type]
         app.logger.info(f"add_rule: ")
         return redirect(url_for('settings'))
 
